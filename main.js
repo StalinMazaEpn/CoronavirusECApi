1f8a0fc8552a8dbe0ba7fc189edb13c8a032620a
StalinMazaEPN <stalinct97@gmail.com>
2020-03-28 22:07:02 -0500

    agregado datos iniciales para el mapa con leaflet

---
+++ main.js
@@ -0,0 +1,154 @@
+let lightLayer = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const  darkIcon = '🌛';
+const  lightIcon = '☀️';
+const  filterDark = ['invert:100%'];
+const  filterLight = [];
+let currentFilter = [];
+let btnIcon = lightIcon;
+const leafletAtribution = '&copy; <a href="https://www.openstreetmap.org/copyright">Gracias a OpenStreetMap</a>';
+const initialCoords = [-1.702630, -78.695068];
+
+//Funciones Manejar Temas
+const getThemeMode = () =>{
+    const mode = localStorage.getItem('sm-mode-theme');
+    console.log('get theme mode value', mode)
+    if(!mode){
+        return 'light'
+    }else{
+        return mode;
+    }
+}
+
+const setThemeMode = (mode) => {
+    localStorage.setItem('sm-mode-theme', mode);
+}
+
+const toggleThemeMode = () => {
+    const mode = getThemeMode();
+    if(mode == 'light'){
+        setThemeMode('dark');
+    }else{
+        setThemeMode('light');
+    }
+}
+
+
+//Verificar Modo
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matchess) {
+    localStorage.setItem('sm-mode-theme', 'dark');
+}
+
+if(localStorage.getItem("sm-mode-theme") == null){
+    localStorage.setItem('sm-mode-theme', 'light');
+}
+
+
+currentFilter = (getThemeMode() == 'light') ? filterLight: filterDark;
+
+
+
+//Crear Mapa
+var map = L.map('map', {
+    zoomAnimation: false,
+    markerZoomAnimation: false,
+    zoomControl: true,
+}).setView(initialCoords, 7);
+//Añadirle una capa
+let mapTileLayer = L.tileLayer.colorFilter(lightLayer, {
+    attribution: leafletAtribution,
+    updateWhenIdle: true,
+    reuseTiles: true,
+    filter: currentFilter,
+}).addTo(map);
+
+
+// Añadir boton FullScreen
+var fsControl = L.control.fullscreen();
+map.addControl(fsControl);
+// Manejar Eventos al Salir y entrar del modo pantalla completa
+// map.on('enterFullscreen', function(){
+//     if(window.console) window.console.log('enterFullscreen');
+// });
+// map.on('exitFullscreen', function(){
+//     if(window.console) window.console.log('exitFullscreen');
+// });
+
+//Añadir Boton Modo Tema
+let btnThemeControl = L.control();
+btnThemeControl.onAdd = function (map) {
+    let container = L.DomUtil.create('input');
+    container.type = "button";
+    container.value = btnIcon;
+    container.title = "Modo Visualización";
+    //Funcion cuando hagan click en el boton de toggle Mode
+    container.onclick = function (event) {
+        toggleThemeMode();
+        //Cambiar Boton, Clase y Filtro de Mapa
+        if(getThemeMode() == 'light'){
+            event.target.value = lightIcon;
+            event.target.classList.remove('dark');
+            mapTileLayer.updateFilter(filterLight);
+        }else{
+            event.target.value = darkIcon;
+            mapTileLayer.updateFilter(filterDark);
+            event.target.classList.add('dark');
+        }
+    }
+    container.classList.add('btnThemeControl')
+    return container;
+};
+btnThemeControl.addTo(map);
+//Funcion para traer los datos de un API
+async function getData() {
+    const response = await fetch('./db.json')
+    const data = await response.json()
+    return data
+}
+//Función renderizar datos
+function renderExtraData({ nombre, confirmados }) {
+    return (`
+        <div>
+          <p> <strong>${nombre}</strong> </p>
+          <p> Confirmados: ${confirmados} </p>
+        </div>
+      `)
+}
+//Añador Titulo Información
+var info = L.control({position:'bottomcenter'});
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Mapa Coronavirus en Ecuador</h4>' + '<span>Datos del MSP del Ecuador</span>';
+};
+info.addTo(map);
+
+//Crear Icono
+const iconUrl = './icon.png';
+const shadowIcon = './marker-shadow.png';
+const icon = new L.Icon({
+    iconUrl: iconUrl,
+    shadowUrl: shadowIcon,
+    shadowSize: [40, 40],
+    iconSize: [40, 40],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+});
+
+//Añadir Marcadores
+async function renderData() {
+    const data = await getData();
+    const provincias = data.provincias;
+    // let markersGroup = [];
+    provincias.forEach((item, index) => {
+        const marker = L.marker([item.latitude, item.longitude], { icon: icon })
+        // .addTo(map)
+        .bindPopup(renderExtraData(item))
+        .addTo(map);
+    });
+}
+
+//Ejecutar la función Inicial
+renderData()
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Coronavirus 2019 EC Mapa
+
+Ejercicio realizado para mostrar un mapa actualizado de las personas afectadas por el coronavirus a nivel de mi Pais Ecuador, usando herramientas como:
+- Leaflet 
+- Leaflet FullScreen
+- Leaflet Edgebuffer
+
+## Como Probarlo
+
+Puedes verlo [aquí](https://coronavirus-ec-sm-2019.netlify.com/) o seguir la guía sobre **Como Utilizarlo**
+
+## ¿Como Utilizarlo?
+
+Puedes bajarte el proyecto y ejecutarlo en un servidor web, si tienes NodeJS puedes instalar el paquete **live-server** y ejecutar el comando
+```cmd
+live-server
+```
+
+## Imágenes
+
+**Versión Móvil**
+![Móvil](./MapaCoronavirusMovil.png)
+
+**Versión Web**
+![Móvil](./MapaCoronavirusWeb.png)
+
+**Versión Web**
+
+## Inspiración
+
+Este proyecto fue inspirando en un video genial que hizo el docente **Leonidas Esteban** y que me motivo a realizarlo con la librería Leaflet de manera gratuíta
+- Pueden ver la clase original [aquí](https://www.youtube.com/watch?v=UlfacaW8634)
+- Pueden revisar el API utilizado en el archivo DB.JSON o [aquí](https://my-json-server.typicode.com/StalinMazaEpn/CoronavirusECApi)
+
+## Autor
+> Stalin Maza - Desarrollador Web
+- Puedes ver mi blog [aquí](https://stalinmaza97.hashnode.dev/)
+- Puedes ver mi canal [aquí](https://www.youtube.com/channel/UCMDvFIXXZv5tUXNa7-qF5pw?view_as=subscriber)
+
+## Nota
+Los datos los tratare de actualizar lo mas pronto posible siguiendo los datos oficiales que provee el Ministerio de Salud Publica de Ecuador que los puedes ver [aquí](https://www.salud.gob.ec/actualizacion-de-casos-de-coronavirus-en-ecuador/)
